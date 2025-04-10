ce198c5b15065b80e07fbc732c0f08b9292e8d82
BonVon <matthew.herrick19@gmail.com>
2025-04-10 18:59:40 -0400

    changed from include_once to require_once

--- api/books/index.php
+++ api/books/index.php
@@ -10,20 +10,20 @@ if($method === 'OPTIONS'){
 }
 
 if($method === "POST") { // POST Request
-    include_once 'create.php';
+    require_once 'create.php';
 } else if ($method === "GET") { // GET Request
     if(isset($_GET['id']) || isset($_GET['title']) ||
         isset($_GET['author']) || isset($_GET['author_name']) || isset($_GET['genre']) ||
         isset($_GET['isbn']) || isset($_GET['year']) ||
         isset($_GET['available'])) {
-        include_once 'get.php';
+        require_once 'get.php';
     } else {
-        include_once 'getALL.php';
+        require_once 'getALL.php';
     }
 } else if ($method === "PUT") { // PUT Request
-    include_once 'update.php';
+    require_once 'update.php';
 } else if ($method === "DELETE") { // DELETE Request
-    include_once 'delete.php';
+    require_once 'delete.php';
 } else {
     echo("Incorrect method being used. It must be either GET, PUT, DELETE, or POST.");
 }
